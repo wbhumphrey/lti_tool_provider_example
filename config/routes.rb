1f79bad96cd0c60c5ad446163efbbfd737856dbc
Nathan Mills <nathanm@instructure.com>
2014-11-03 11:20:47 -0700

    add root landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 LtiToolProvider::Application.routes.draw do
+  root to: 'guide#home'
+
   resources :tool_proxy, only: [:create]
 
   post 'messages/blti', to: 'message#basic_lti_launch_request'
 
-  post 'register', to: 'registration#register'
+  post 'register', to: 'registration#register', as: :tool_registration
   post 'submit_capabilities', to: 'registration#save_capabilities', as: 'save_capabilities'
 
   mount RailsLti2Provider::Engine => "/rails_lti2_provider"
