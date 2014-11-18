67c31c7e88811ea1bbffad16aaa5529e613b963f
Nathan Mills <nathanm@instructure.com>
2014-11-18 09:28:43 -0500

    handle unauthorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from RailsLti2Provider::LtiLaunch::Unauthorized do |ex|
+    render :nothing => true, :status => 401
+  end
+
 end
